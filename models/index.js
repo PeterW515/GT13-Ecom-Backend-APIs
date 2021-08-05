4d5f9d337f091cd43a200ee6cab3d50423039a87
Peter Weigman <phweigman@gmail.com>
2021-08-04 22:18:11 -0400

    added model relations

--- models/index.js
+++ models/index.js
@@ -4,14 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'categoryId'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey:'categoryId',
+  onDelete:'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through:ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through:ProductTag,
+});
 module.exports = {
   Product,
   Category,
